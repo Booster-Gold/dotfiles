499c23cc84da5ce749dc05bd455ce3e026b5ae3c
Bob <slowburn84@gmail.com>
2021-01-05 03:10:00 -0800

    Dunno

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+
+# Powerline setup
+$global:DefaultUser = [System.Environment]::UserName
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Sorin
+
+# Set working directory to personal drive
+
+Set-Location E:\Bob
+
+# Alias
